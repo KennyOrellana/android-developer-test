33f789632647141cc889dfb903a6e8429ff0308c
Kenny Orellana <kenny.orellana504@gmail.com>
2020-07-22 21:54:53 -0600

    Base Project

---
+++ app/src/main/java/app/kaisa/parky/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.kaisa.parky
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
